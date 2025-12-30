5e1af729c21285abfd131e647a75fd46e0142ec3
miraai-labs <daun7220@mail.ru>
2025-12-30 09:20:02 +0500

    Update storage.py
    make function for save tasks

--- storage.py
+++ storage.py
@@ -6,3 +6,6 @@ def load_data():
     with open(file_name,"r", encoding = "utf-8") as file:
         return json.load(file)
     
+def save_data():
+    with open(file_name,"w",encoding = "utf-8") as file:
+        return json.dump(file,ensure_ascii=False,indent=2)   
