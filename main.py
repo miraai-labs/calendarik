8b06c14e09cec747b8feebbd248cc8b7d499e299
miraai-labs <daun7220@mail.ru>
2025-12-31 03:59:20 +0500

    Update main.py
    create nachalo  interfeica calendarya

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ def show_menu():
     print('3. Отметить задачу выполненной')
     print('4. Удалить задачи')
     print('0. Выход')
+def main(): 
+    while True:
+        show_menu()
+        otvet = input("Выберите пункт: ")
+        if otvet == "0":
+            print("Программа завершена")
+            break
+        if otvet == "1":
+            print('Показать задачи')
+        if otvet == "2":
+            print("Добавить задачи")
+        if otvet == "3":
+            print("Отметить задачи выполненной")
+        if otvet == "4":
+            print("Удалить задачи")
+        else:
+            print("Не корректный выбор,повторите попытку")
+      
